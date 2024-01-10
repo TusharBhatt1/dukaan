599f9fbb11de62d56b666ccbd9d19b29cb9bf7dd
Tushar Bhatt <tusharbhatt0135@gmail.com>
2024-01-10 08:44:50 +0530

    reponsive-design

--- src/Components/right/Transactions.tsx
+++ src/Components/right/Transactions.tsx
@@ -15,15 +15,15 @@ export default function Transactions() {
       <p className="font-extrabold">Transactions | This Month</p>
 
       <div className="mt-2 text-sm flex flex-col bg-white">
-        <div className="grid grid-cols-4 text-black text-xs text-slate-400 bg-slate-200 p-2">
+        <div className="grid grid-cols-4  text-xs text-slate-400 bg-slate-200 p-2">
           <p>Order ID</p>
           <p>Order Date</p>
           <p>Order amount</p>
           <p>Transaction Fees</p>
         </div>
-        <div className="flex flex-col gap-2 p-2">
+        <div className="flex flex-col gap-2 p-2 text-xs md:text-md">
           {allOrders.map((order) => (
-            <div key={order.orderId} className="grid grid-cols-4 py-1 hover:bg-slate-100 cursor-pointer">
+            <div key={order.orderId} className="grid grid-cols-4 gap-2 md:gap-0 py-1 hover:bg-slate-100 cursor-pointer">
               <p className="text-blue-500">#{order.orderId}</p>
               <p>{formatDate(order.orderDate)}</p>
               <p>&#8377; {order.orderAmount}</p>
