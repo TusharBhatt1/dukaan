0cb815d88145773f9cb892804b3007ca8fbb69cd
Tushar Bhatt <tusharbhatt0135@gmail.com>
2024-01-11 12:15:35 +0530

    new text

--- src/Components/Intro.tsx
+++ src/Components/Intro.tsx
@@ -10,7 +10,7 @@ export default function Intro() {
         <div className="text-white flex flex-col gap-4 ">
           <div>
             <p className="text-blue-500 font-bold">
-              A Responsive React + Typescript + Zustand + Tailwind project by
+              A Completely Responsive React + Typescript + Zustand + Tailwind project by
             </p>
             <p>Tushar Bhatt</p>
           </div>
