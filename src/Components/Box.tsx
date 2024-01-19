44fa99f0442c4c79fb9225227461cd85eeca1dc0
Tushar Bhatt <tusharbhatt0135@gmail.com>
2024-01-19 14:35:28 +0530

    change - [text color]

--- src/Components/Box.tsx
+++ src/Components/Box.tsx
@@ -8,7 +8,7 @@ type?: string | "price"
 export default function Box({title,content,type}:BoxProps) {
   return (
     <div className="flex flex-col text-start gap-2 p-2 bg-white w-[30vw]">
-    <p className="text-slate-300">{title}</p>
+    <p className="text-slate-400">{title}</p>
     <p className="font-bold">{type === "price" && "Rs. "}{content}</p>
     </div>
   )
