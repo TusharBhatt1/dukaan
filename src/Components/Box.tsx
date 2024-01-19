faec9dc3c044a280cbc47e2c514209cd4d7d5281
Tushar Bhatt <tusharbhatt0135@gmail.com>
2024-01-19 14:35:42 +0530

    padding

--- src/Components/Box.tsx
+++ src/Components/Box.tsx
@@ -7,7 +7,7 @@ type?: string | "price"
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 export default function Box({title,content,type}:BoxProps) {
   return (
-    <div className="flex flex-col text-start gap-2 p-2 bg-white w-[30vw]">
+    <div className="flex flex-col text-start gap-2 p-3 bg-white w-[30vw]">
     <p className="text-slate-400">{title}</p>
     <p className="font-bold">{type === "price" && "Rs. "}{content}</p>
     </div>
